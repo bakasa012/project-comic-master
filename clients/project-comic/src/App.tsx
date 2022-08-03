33b897ceb9464154f932f7ed2f432dbd43b87dd6
GEO\lieu.hong.thai <lieu.hong.thai@geonet.co.jp>
2022-08-03 17:00:27 +0700

    setup animation

--- clients/project-comic/src/App.tsx
+++ clients/project-comic/src/App.tsx
@@ -3,21 +3,24 @@ import './App.css';
 import 'antd/dist/antd.min.css';
 import ConfigProviderComponent from './andt.configs/ConfigProvider.component.config';
 import { Layout } from 'antd';
+import Home from './layouts/demos/Home';
 
 function App() {
   const { Header, Footer, Content } = Layout;
   return (
-    <div className="App">
-      <ConfigProviderComponent>
-        <>
-          <Layout>
-            <Header>Header</Header>
-            <Content>Content</Content>
-            <Footer>Footer</Footer>
-          </Layout>
-        </>
-      </ConfigProviderComponent>
-    </div>
+    // <div className="App">
+    //   <ConfigProviderComponent>
+    //     <>
+    //       <Layout>
+    //         <Header>Header</Header>
+    //         <Content style={{ height: '100vh' }}>
+    //         </Content>
+    //         <Footer>Footer</Footer>
+    //       </Layout>
+    //     </>
+    //   </ConfigProviderComponent>
+    // </div>
+    <Home></Home>
   );
 }
 
