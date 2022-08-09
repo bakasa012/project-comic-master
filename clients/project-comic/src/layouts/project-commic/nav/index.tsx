3cf31af911fddbfeb81cb7ad6995e465026aaba0
lieu.hong.thai <lieu.hong.thai@geonet.co.jp>
2022-08-09 21:48:35 +0700

    update code

--- clients/project-comic/src/layouts/project-commic/nav/index.tsx
+++ clients/project-comic/src/layouts/project-commic/nav/index.tsx
@@ -1,12 +1,27 @@
-import { Layout } from 'antd';
+import { Layout, MenuProps } from 'antd';
 import TweenOne from 'rc-tween-one';
 import React from 'react';
-
+import './nav.scss';
+type MenuItem = Required<MenuProps>['items'][number];
 const NavComponent = () => {
-	const { Header } = Layout
+	const { Header } = Layout;
+
+	const getItemMenu = (
+		label: React.ReactNode,
+		key: React.Key,
+		icon?: React.ReactNode,
+		children?: MenuItem[],
+		type?: 'group',
+	): MenuItem => ({
+		key,
+		icon,
+		children,
+		label,
+		type,
+	});
 	return (
 		<Header>
-			<TweenOne component={'header'} animation={{ opacity: 0, type: 'from' }} className='header0 home-page-wrapper'>
+			<TweenOne component={'header'} animation={{ opacity: 1, type: 'from' }} className='header0 home-page-wrapper'>
 				<TweenOne animation={{ x: -30, type: 'from', ease: 'easeOutQuad' }} className='header0-logo'>
 					<img src='https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg' alt='img' />
 				</TweenOne>
