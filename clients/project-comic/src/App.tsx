1d10e70d6175fcaba1b695c692e1906fe28ef7fd
GEO\lieu.hong.thai <lieu.hong.thai@geonet.co.jp>
2022-08-10 17:32:09 +0700

    update code

--- clients/project-comic/src/App.tsx
+++ clients/project-comic/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.min.css';
-import ConfigProviderComponent from './andt.configs/ConfigProvider.component.config';
-import { Layout } from 'antd';
-import Home from './layouts/demos/Home';
+// import ConfigProviderComponent from './andt.configs/ConfigProvider.component.config';
+// import { Layout } from 'antd';
+// import Home from './layouts/demos/Home';
 import ProjectComic from './layouts/project-commic';
 
 function App() {
-  const { Header, Footer, Content } = Layout;
+  // const { Header, Footer, Content } = Layout;
   return (
     // <div className="App">
     //   <ConfigProviderComponent>
--- clients/project-comic/src/layouts/project-commic/nav/index.tsx
+++ clients/project-comic/src/layouts/project-commic/nav/index.tsx
@@ -1,36 +1,41 @@
 import { Layout, MenuProps } from 'antd';
 import TweenOne from 'rc-tween-one';
 import React from 'react';
+import { Nav00DataSource } from '../../demos/Home/data.source';
 import './nav.scss';
 type MenuItem = Required<MenuProps>['items'][number];
 const NavComponent = () => {
-	const { Header } = Layout;
+  const { Header } = Layout;
 
-	const getItemMenu = (
-		label: React.ReactNode,
-		key: React.Key,
-		icon?: React.ReactNode,
-		children?: MenuItem[],
-		type?: 'group',
-	): MenuItem => ({
-		key,
-		icon,
-		children,
-		label,
-		type,
-	});
-	const items = [
-
-	]
-	return (
-		<Header>
-			<TweenOne component={'header'} animation={{ opacity: 1, type: 'from' }} className='header0 home-page-wrapper'>
-				<TweenOne animation={{ x: -30, type: 'from', ease: 'easeOutQuad' }} className='header0-logo'>
-					<img src='https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg' alt='img' />
-				</TweenOne>
-			</TweenOne>
-		</Header>
-	);
+  const getItemMenu = (
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+    type?: 'group',
+    className?: string,
+  ): MenuItem => ({
+    key,
+    icon,
+    children,
+    label,
+    type,
+    className,
+  });
+  const items = [
+    ...Nav00DataSource.Menu.children.map((child, index: number) => {
+      if (child) return;
+    }),
+  ];
+  return (
+    <Header>
+      <TweenOne component={'header'} animation={{ opacity: 1, type: 'from' }} className="header0 home-page-wrapper">
+        <TweenOne animation={{ x: -30, type: 'from', ease: 'easeOutQuad' }} className="header0-logo">
+          <img src="https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg" alt="img" />
+        </TweenOne>
+      </TweenOne>
+    </Header>
+  );
 };
 
 export default NavComponent;
