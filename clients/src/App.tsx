900984631953c31b4476737a736f06b1e807fc92
GEO\lieu.hong.thai <51442823+bakasa012@users.noreply.github.com>
2022-10-11 11:26:52 +0700

    update folder

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.min.css';
-// import ConfigProviderComponent from './andt.configs/ConfigProvider.component.config';
 // import { Layout } from 'antd';
 // import Home from './layouts/demos/Home';
 // import ProjectComic from './layouts/project-commic';
