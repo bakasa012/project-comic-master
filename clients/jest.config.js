63c5112e109962997c73e5bdcb21e2e60eb14cfd
GEO\lieu.hong.thai <51442823+bakasa012@users.noreply.github.com>
2022-11-09 17:08:09 +0700

    update code .gitignore

--- clients/jest.config.js
+++ clients/jest.config.js
@@ -3,13 +3,13 @@ module.exports = {
 	roots: [ 'src/tests' ],
 	testTimeout: 30000,
 	collectCoverage: false,
-	coverageDirectory: "<rootDir>/test-reports/coverage",
+	coverageDirectory: "<rootDir>/.report-automation-test/coverage",
 	coverageReporters: ["text", "json", "html"],
 	"reporters": [
 		"default",
 		["./node_modules/jest-html-reporter", {
 			"pageTitle": "Project-comic-app",
-			"outputPath": "test-reports/index.html",
+			"outputPath": ".report-automation-test/index.html",
 			"includeFailureMsg": true
 		}]
 	]
